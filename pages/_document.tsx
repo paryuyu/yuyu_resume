86243bacdbd3edf5a8e8c612823294ac4dd56db0
paryuyu <107971288+paryuyu@users.noreply.github.com>
2023-02-02 11:35:16 +0900

    미리보기 이미지

---
+++ pages/_document.tsx
@@ -0,0 +1,9 @@
+import { Head, Html } from "next/document";
+
+export default function Document (){
+  <Html>
+    <Head>
+      <meta name="image" property="og:image" content="/images/preview.jpg"/>
+    </Head>
+  </Html>
+}
